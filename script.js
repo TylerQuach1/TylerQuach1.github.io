276f53ca6ae9c8b2003b0512e4b154a8686d0506
Tyler Quach <59546424+TylerQuach1@users.noreply.github.com>
2020-11-03 22:59:47 -0500

    Added smoother collapse functionality

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-console.log("false")
-
 function collapse(id){
-    console.log("true")
     if (document.getElementById(id).style.getPropertyValue("overflow") == "hidden"){
         document.getElementById(id).style.setProperty("overflow", "auto")
-        document.getElementById(id).style.setProperty("height", "100vh")
+        document.getElementById(id).style.setProperty("height", "90vh")
+        document.querySelector(".project").style.setProperty("padding-top", "0")
     }
     else{
         document.getElementById(id).style.setProperty("overflow", "hidden")
         document.getElementById(id).style.setProperty("height", "0%")
+        document.querySelector(".project").style.setProperty("padding-top", "45vh")
     }
 }

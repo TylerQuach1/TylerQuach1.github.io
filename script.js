3bacacdad9761a0bb55e9e163446f0bdf3bffa84
Tyler Quach <59546424+TylerQuach1@users.noreply.github.com>
2021-01-05 15:15:44 -0500

    Finalized Website
    Added hover animation and functionality with intersection observer

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
+const hovering = document.querySelectorAll('.hover');
+
+const options = {
+    root: null,
+    treshold: 1,
+    rootMargin: "-50% 0% -50% -50%",
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("hover");
+    })
+}, options);
+
+hovering.forEach(hovering =>{
+    observer.observe(hovering);
+})
+
+
 function scrolling(){
-    var point = document.querySelector(".project").scrollTop
-    console.log("zoowee")
-    console.log(point)
-    if (point >= 890){
-        document.querySelector(".project").scrollTop = 890
+    var point = document.querySelector(".project").scrollTop;
+    if (point >= 900){
+        document.querySelector(".project").scrollTop = 900;
     }
 }
 
-
 function collapse(id){
-    if (document.getElementById(id).style.getPropertyValue("overflow") == "hidden"){
+    if (document.getElementById(id).style.getPropertyValue("overflow") == "hidden" && document.getElementById(id).previousElementSibling.classList.contains("hover")){
         document.getElementById(id).style.setProperty("overflow", "auto");
-        document.getElementById(id).style.setProperty("height", "90vh");
+        document.getElementById(id).style.setProperty("height", "100vh");
         document.querySelector(".project").style.setProperty("padding-top", "0vh");
         document.querySelector(".project").style.setProperty("overflow", "hidden");
         document.querySelector(".sel_arrow").style.setProperty("border-left", "0px solid #FF7F50");
     }
     else{
         document.getElementById(id).style.setProperty("overflow", "hidden");
-        document.getElementById(id).style.setProperty("height", "0");
+        document.getElementById(id).style.setProperty("height", "0vh");
         document.querySelector(".project").style.setProperty("padding-top", "45vh");
         document.querySelector(".project").style.setProperty("overflow","auto");
         document.querySelector(".sel_arrow").style.setProperty("border-left", "50px solid #FF7F50");
     }
 }
+
